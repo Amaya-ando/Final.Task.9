2b3a789bed8a2244a1c792c22ce684561390d248
Victoria <theviktori39@gmail.com>
2024-03-08 01:57:53 +0300

    Добавьте файлы проекта.

---
+++ Task1/Task1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.Task._9.Task1
+{
+    class Program1
+    {
+        public static void Main(string[] args)
+        {
+            Task1 task1 = new Task1();
+            task1.TaskException();
+        }
+
+    }
+
+    public class MyException : Exception
+    {
+        public MyException()
+        {
+
+        }
+
+        public MyException(string message) : base(message)
+        {
+
+
+        }
+
+    }
+
+    public class Task1
+    {
+        internal void TaskException()
+        {
+            Exception[] exceptions = new Exception[5];
+            exceptions[0] = new MyException("Некорректный возраст");
+            exceptions[1] = new ArgumentNullException("Аргумент, передаваемый в метод — null");
+            exceptions[2] = new DivideByZeroException("Деление на ноль");
+            exceptions[3] = new IndexOutOfRangeException("Индекс вне заданных пределов");
+            exceptions[4] = new ArgumentOutOfRangeException("Аргумент вне заданных пределов");
+
+            foreach (Exception exception in exceptions)
+            {
+                try
+                {
+                    throw exception;
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Task2/Task2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Final.Task._9.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEneteredEvent += ShowNumber;
+
+            while (true)
+            {
+                try
+                {
+                    numberReader.Read();
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введено некорректное значение!");
+                }
+
+            }
+
+        }
+
+        static void ShowNumber(int number)
+        {
+            string[] strings = { "Зайцев", "Фролов", "Васькин", "Капустин", "Пирожков" };
+
+            switch (number)
+            {
+                case 1:
+                    Array.Sort(strings);
+                    string sr = string.Join(",", strings);
+                    Console.WriteLine(sr);
+                    break;
+                case 2:
+                    Array.Sort(strings);
+                    Array.Reverse(strings);
+                    string str = string.Join(",", strings);
+                    Console.WriteLine(str);
+                    break;
+
+            }
+
+        }
+    }
+    class NumberReader
+    {
+        public delegate void NumberEnteredDelegate(int number);
+        public event NumberEnteredDelegate NumberEneteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine("Необходимо ввести значение: либо 1, либо 2");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) throw new FormatException();
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEneteredEvent?.Invoke(number);
+        }
+    }
+}
